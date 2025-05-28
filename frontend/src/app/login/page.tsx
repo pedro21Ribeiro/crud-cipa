dcd184abc15e1fe619a704f952d4cf2916dc9be5
Joao Murilo <j.murilo.mobile@gmail.com>
2025-05-28 13:28:44 -0300

    criaçao da logica e aplicaçao da api no front -a aplicaçao da api esta como 'teste', nao é uma api valida -aguardando a criaçao de endpoint para criaçao de usuario para validar -add mensagem de erro caso o usuario nao esteja registrado no banco -mensagem de erro para caso email ou senha esteja incorretos

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,16 +1,56 @@
-import React from "react";
+"use client"; // Adicione esta linha no topo do arquivo
+
+import React, { useState } from "react"; // Importar useState
 import Link from "next/link";
 import TextField from "@mui/material/TextField";
 import { Button, Card, Typography } from "@mui/material";
 
 export default function Login() {
+  const [email, setEmail] = useState(""); // Estado para o email
+  const [password, setPassword] = useState(""); // Estado para a senha
+  const [error, setError] = useState(""); // Estado para erros
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // Impede o comportamento padrão do formulário
+
+    // Verifica se os campos estão preenchidos
+    if (!email || !password) {
+      setError("Por favor, preencha todos os campos.");
+      return;
+    }
+
+    // Aqui você deve adicionar a lógica de autenticação
+    try {
+      const response = await fetch("/api/login", {
+        // coloque o endpoint aqui
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Email ou senha incorretos.");
+      }
+
+      setError("");
+    } catch (err) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError("Ocorreu um erro desconhecido.");
+      }
+    }
+  };
+
   return (
     <div
       style={{
         display: "flex",
-        justifyContent: "center", // Centralizar horizontalmente
-        alignItems: "center", // Centralizar verticalmente
-        height: "90vh", // Definir a altura do contêiner para 100% da altura da janela
+        justifyContent: "center",
+        alignItems: "center",
+        height: "90vh",
       }}
     >
       <Card
@@ -32,7 +72,8 @@ export default function Login() {
             Digite seus dados de acesso no campo a baixo
           </Typography>
         </div>
-        <form>
+        <form onSubmit={handleLogin}>
+          {" "}
           <Typography
             variant="h6"
             sx={{
@@ -48,14 +89,15 @@ export default function Login() {
             variant="outlined"
             fullWidth
             margin="normal"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             sx={{
-              backgroundColor: "rgba(230, 230, 230, 1)", // Cor de fundo
-              border: "0", // Remover borda
-              borderRadius: "15px", // Borda arredondada
+              backgroundColor: "rgba(230, 230, 230, 1)",
+              border: "0",
+              borderRadius: "15px",
               "& .MuiOutlinedInput-root": {
-                // Estilo para o root do input
                 "& fieldset": {
-                  border: "none", // Remover o fieldset (bordas)
+                  border: "none",
                 },
               },
             }}
@@ -76,25 +118,24 @@ export default function Login() {
             variant="outlined"
             fullWidth
             margin="normal"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             sx={{
-              backgroundColor: "rgba(230, 230, 230, 1)", // Cor de fundo
-              border: "0", // Remover borda
-              borderRadius: "15px", // Borda arredondada
+              backgroundColor: "rgba(230, 230, 230, 1)",
+              border: "0",
+              borderRadius: "15px",
               "& .MuiOutlinedInput-root": {
-                // Estilo para o root do input
                 "& fieldset": {
-                  border: "none", // Remover o fieldset (bordas)
+                  border: "none",
                 },
               },
             }}
           />
-        </form>
-
-        <Link href={"/home"} style={{ textDecoration: "none" }}>
           <div style={{ padding: "10px" }}>
             <Button
               variant="contained"
               fullWidth
+              type="submit"
               sx={{
                 margin: "auto",
                 display: "flex",
@@ -115,9 +156,12 @@ export default function Login() {
               </Typography>
             </Button>
           </div>
-        </Link>
+          <div style={{ padding: "5px", textAlign: "center" }}>
+            {error && <Typography sx={{ color: "red" }}>{error}</Typography>}{" "}
+          </div>
+        </form>
         <Typography sx={{ textAlign: "center", paddingTop: "10px" }}>
-          Não tem conta ?{" "}
+          Não tem conta?{" "}
           <Link href="/register" style={{ textDecoration: "none" }}>
             faça seu registro
           </Link>
